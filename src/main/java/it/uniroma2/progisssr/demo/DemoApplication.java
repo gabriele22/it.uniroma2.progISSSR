b201255a2d03a6ae1c9e111dcf27c260b2342fc2
gabriele22 <gabtuni@gmail.com>
2018-05-10 16:39:33 +0200

    primo

---
+++ src/main/java/it/uniroma2/progisssr/demo/DemoApplication.java
@@ -0,0 +1,12 @@
+package it.uniroma2.progisssr.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
+}
