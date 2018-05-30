8a0ecdc6e8451518548788e081cd459bfe03fec7
Melissa95 <melissaceli71@gmail.com>
2018-05-30 17:34:54 +0200

    target

--- src/main/java/it/uniroma2/progisssr/controller/TargetController.java
+++ src/main/java/it/uniroma2/progisssr/controller/TargetController.java
@@ -1,6 +1,6 @@
 package it.uniroma2.progisssr.controller;
 
-import it.uniroma2.progisssr.dao.ProductDao;
+import it.uniroma2.progisssr.dao.TargetDao;
 import it.uniroma2.progisssr.entity.Target;
 import it.uniroma2.progisssr.exception.EntitaNonTrovataException;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,47 +8,46 @@ import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
 import javax.validation.constraints.NotNull;
-import java.util.ArrayList;
 import java.util.List;
 
 @Service
 public class TargetController {
 
     @Autowired
-    ProductDao productDao;
+    TargetDao targetDao;
 
     @Transactional
     public @NotNull Target createTarget(@NotNull Target target) {
-        Target newTarget = productDao.save(target);
+        Target newTarget = targetDao.save(target);
         return newTarget;
     }
     @Transactional
     public @NotNull Target updateTarget( @NotNull Long id, @NotNull Target target) throws EntitaNonTrovataException{
 
-        Target targetToUpdate = productDao.getOne(id);
+        Target targetToUpdate = targetDao.getOne(id);
         if (targetToUpdate == null)
             throw new EntitaNonTrovataException();
         targetToUpdate.update(target);
-        Target targetUpdated = productDao.save(targetToUpdate);
+        Target targetUpdated = targetDao.save(targetToUpdate);
         return targetUpdated;
 
     }
 
     public Target findTargetById(@NotNull Long id) {
-        Target target = productDao.getOne(id);
+        Target target = targetDao.getOne(id);
         return target;
     }
 
     public boolean deleteTarget(@NotNull Long id){
-        if(!productDao.existsById(id)){
+        if(!targetDao.existsById(id)){
             return false;
         }
-        productDao.deleteById(id);
+        targetDao.deleteById(id);
         return true;
     }
 
     public List<Target> findAllTickets(){
-        List<Target> targets = productDao.findAll();
+        List<Target> targets = targetDao.findAll();
         return targets;
     }
 }
--- src/main/java/it/uniroma2/progisssr/dao/TargetDao.java
+++ src/main/java/it/uniroma2/progisssr/dao/TargetDao.java
@@ -3,6 +3,6 @@ package it.uniroma2.progisssr.dao;
 import it.uniroma2.progisssr.entity.Target;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ProductDao extends JpaRepository<Target,Long> {
+public interface TargetDao extends JpaRepository<Target,Long> {
 
 }
